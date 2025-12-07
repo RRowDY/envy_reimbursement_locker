5e61fa46ed4e7453d7e84e8f5f556283223be80c
RRowDY <durellwalzer@gmail.com>
2025-12-07 00:48:43 -0700

    cache DOM elements and optimize color parsing

--- html/script.js
+++ html/script.js
@@ -1,70 +1,74 @@
 let isVisible = false;
 
+const DOM = {
+    ui: null,
+    text: null
+};
+
+function getDOM() {
+    if (!DOM.ui) {
+        DOM.ui = document.getElementById('locker-ui');
+        DOM.text = document.getElementById('locker-text');
+    }
+    return DOM;
+}
+
 function updatePosition(ui, x, y) {
     ui.style.left = x + 'px';
     ui.style.top = y + 'px';
 }
 
+const COLOR_MAP = {
+    '~r~': '<span style="color: #ff4444;">',
+    '~g~': '<span style="color: #44ff44;">',
+    '~b~': '<span style="color: #00ffff;">',
+    '~y~': '<span style="color: #ffff44;">',
+    '~p~': '<span style="color: #ff44ff;">',
+    '~c~': '<span style="color: #cccccc;">',
+    '~m~': '<span style="color: #888888;">',
+    '~u~': '<span style="color: #000000;">',
+    '~o~': '<span style="color: #ff8844;">',
+    '~eb~': '<span style="color: #0987ff;">',
+    '~s~': '</span>',
+    '~w~': '</span>'
+};
+
+const COLOR_REGEX = new RegExp(Object.keys(COLOR_MAP).map(code => code.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')).join('|'), 'g');
+
 function parseColorCodes(text) {
     if (!text) return '';
-    
-    const colorMap = {
-        '~r~': '<span style="color: #ff4444;">',
-        '~g~': '<span style="color: #44ff44;">',
-        '~b~': '<span style="color: #00ffff;">',
-        '~y~': '<span style="color: #ffff44;">',
-        '~p~': '<span style="color: #ff44ff;">',
-        '~c~': '<span style="color: #cccccc;">',
-        '~m~': '<span style="color: #888888;">',
-        '~u~': '<span style="color: #000000;">',
-        '~o~': '<span style="color: #ff8844;">',
-        '~eb~': '<span style="color: #0987ff;">',
-        '~s~': '</span>',
-        '~w~': '</span>'
-    };
-    
-    let htmlText = text;
-    for (const [code, replacement] of Object.entries(colorMap)) {
-        htmlText = htmlText.replace(new RegExp(code.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'), 'g'), replacement);
-    }
-    
-    return htmlText;
+    return text.replace(COLOR_REGEX, (match) => COLOR_MAP[match] || match);
 }
 
 window.addEventListener('message', function(event) {
     const data = event.data;
     
-    if (data.action === 'showAdmin' || data.action === 'hideAdmin' || data.action === 'characters') {
+    if (!data || !data.action || data.action === 'showAdmin' || data.action === 'hideAdmin' || data.action === 'characters') {
         return;
     }
     
-    const ui = document.getElementById('locker-ui');
-    const text = document.getElementById('locker-text');
+    const dom = getDOM();
+    if (!dom.ui || !dom.text) return;
     
     if (data.action === 'show') {
         if (data.text) {
-            text.innerHTML = parseColorCodes(data.text);
+            dom.text.innerHTML = parseColorCodes(data.text);
         }
         
         if (data.x !== undefined && data.y !== undefined) {
-            updatePosition(ui, data.x, data.y);
+            updatePosition(dom.ui, data.x, data.y);
         }
         
-        if (data.opacity !== undefined) {
-            ui.style.opacity = data.opacity;
-        } else {
-            ui.style.opacity = '1';
-        }
+        dom.ui.style.opacity = data.opacity !== undefined ? data.opacity : '1';
         
         if (!isVisible) {
-            ui.classList.remove('hidden');
+            dom.ui.classList.remove('hidden');
             isVisible = true;
         }
     } else if (data.action === 'hide') {
         if (isVisible) {
-            ui.classList.add('hidden');
+            dom.ui.classList.add('hidden');
             isVisible = false;
         }
     }
 });
-
